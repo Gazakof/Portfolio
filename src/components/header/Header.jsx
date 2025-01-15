cb4f6c73810f33c09e51abd01f30cbe5ccf71c9d
Gazakof <randriamorasata08@gmail.com>
2025-01-15 20:06:52 +0100

    header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,53 @@
 import React from "react";
+import "./header.css";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="header">
+      <nav className="nav container">
+        <a href="index.html" className="nav__logo">
+          Ezra
+        </a>
+        <div className="nav__menu">
+          <ul className="nav__list grid">
+            <li className="nav__item">
+              <a href="#home" className="nav__link">
+                !icon! Home
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="#about" className="nav__link">
+                !icon! About
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="#skills" className="nav__link">
+                !icon! Skills
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="#services" className="nav__link">
+                !icon! Services
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="#portfolio" className="nav__link">
+                !icon! Portfolio
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="#contact" className="nav__link">
+                !icon! Contact
+              </a>
+            </li>
+          </ul>
+          !icon!
+        </div>
+
+        <div className="nav__toggle">!icon!</div>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
