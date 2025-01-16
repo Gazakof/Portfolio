7ef4985dc987c51362226966279ec5aa0f880354
Gazakof <randriamorasata08@gmail.com>
2025-01-16 10:07:19 +0100

    social jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import Header from "./components/header/Header";
 import "./App.css";
+import Home from "./components/home/Home";
 
 const App = () => {
   return (
     <div>
       <Header />
+
+      <main>
+        <Home />
+      </main>
     </div>
   );
 };
---
+++ src/components/home/Social.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa6";
+
+const Social = () => {
+  return (
+    <div className="home__social">
+      <a href="" className="home__social-icon" target="_blank">
+        <FaInstagram />
+      </a>
+      <a href="" className="home__social-icon" target="_blank">
+        <FaLinkedin />
+      </a>
+      <a href="" className="home__social-icon" target="_blank">
+        <FaGithub />
+      </a>
+    </div>
+  );
+};
+
+export default Social;
